c72d67a8861344c5bb3c4d85317241b14f8b1f81
Travers Biddle <traversbiddle@gmail.com>
2021-09-05 20:24:29 +1200

    Fix error message

--- src/renderer/views/About.vue
+++ src/renderer/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <span>{{errMsg}}</span>
     <input v-model="a" /> + <input v-model="b" /> = <span>{{ans}}</span>
+    <div>{{errMsg}}</div>
   </div>
 </template>
 
@@ -19,6 +19,7 @@ export default class About extends Vue {
 
 	@Watch("a")
 	onAChanged(val: string) {
+		this.errMsg = "";
 		const na = parseInt(val);
 		const nb = parseInt(this.b);
 		Proc.call(na, nb).then((res: any) => {
