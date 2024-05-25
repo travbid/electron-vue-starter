6a1f44b41f67a7640933c3fca573cd9051b6fb52
Travers Biddle <traversbiddle@gmail.com>
2024-05-25 15:55:10 +1200

    style: Formatting

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -1,30 +1,30 @@
-import { createMemoryHistory, createRouter, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import { createMemoryHistory, createRouter, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 const routes: Array<RouteRecordRaw> = [
 	{
-		path: '/',
-		name: 'Home',
-		component: Home
+		path: "/",
+		name: "Home",
+		component: Home,
 	}, {
-		path: '/about',
-		name: 'About',
+		path: "/about",
+		name: "About",
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		// component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-		component: About
+		component: About,
 	}, {
-		path: '/:pathMatch(.*)',
-		redirect: '/'
-	}
-]
+		path: "/:pathMatch(.*)",
+		redirect: "/",
+	},
+];
 
 const router = createRouter({
 	history: createMemoryHistory(),
 	// base: process.env.BASE_URL,
-	routes: routes
-})
+	routes: routes,
+});
 
-export default router
+export default router;
--- src/renderer/shims-vue.d.ts
+++ src/renderer/shims-vue.d.ts
@@ -1,4 +1,4 @@
-declare module '*.vue' {
+declare module "*.vue" {
   import { defineComponent } from "vue";
   const component: ReturnType<typeof defineComponent>;
   export default component;
